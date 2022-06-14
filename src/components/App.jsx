364df7631f7a1b4c6b1aeab5155344c79d34a7ee
Binh <kbinhnguyen.nihongo@gmail.com>
2022-06-14 14:38:17 -0400

    Basic requirements met. Yet to implement API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,56 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+/* TO-DOS:
+
+ Import exampleVideoData.js into your App component so it can be used by your React components
+ Pass exampleVideoData into the VideoList component
+ Refactor the VideoList component to dynamically render one VideoListEntry component for each video object in exampleVideoData
+ Refactor the VideoListEntry component to dynamically render based on the video object it receives
+ Make sure the tests for VideoList and VideoListEntry are passing. You can open the tests with npm test
+
+*/
+
+/* Note: for some reason when import it wants .js files, not .jsx files!!! */
+
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+import VideoListEntry from './VideoListEntry.js';
+import VideoPlayer from './VideoPlayer.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: exampleVideoData,
+      selected: exampleVideoData[0]
+    };
+  }
+
+  onSelect(video) {
+    this.setState({
+      selected: video
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><VideoPlayer video={this.state.selected} /></h5></div>
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videos} onSelect={this.onSelect.bind(this)}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
